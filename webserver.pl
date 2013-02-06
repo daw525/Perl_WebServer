fcb44bd372cccbd8766cd6e61f50c9e17485a366
David Wilson <david.wilson@ishidaeurope.com>
2013-02-06 16:55:25 +0000

    Serve local html files

--- webserver.pl
+++ webserver.pl
@@ -21,18 +21,27 @@ while (my $client = $server->accept()) {
 		last if (/^\r\n/);
 	}
 	if ($string=~/^GET/) {
-		print $string;
-		open (FILE1, "index.html") || die "Can't open html";
-		$page="";
-		while (<FILE1>) {
-			$page=$page.$_;
+		my @p=$string=~(/GET \/(.*?) HTTP/);
+		if ($p[0] eq "") {$p[0]="index.html";}
+		if (-e $p[0]) {
+			open (FILE1, $p[0]) || die "Can't open html";
+			$page="";
+			while (<FILE1>) {
+				$page=$page.$_;
+			}
+			close FILE1;
+			print $client "HTTP/1.0 200 OK\r\n";
+			print $client "Content-Type: text/html; charset=utf-8\r\n";
+			printf $client "Content-Length: %d\r\n",length($page);
+			print $client "\r\n";
+			print $client $page;
+		} else {
+			print $client "HTTP/1.0 404 Not Found\r\n";	
+			print $client "Content-Type: text/html; charset=utf-8\r\n";
+			print $client "Content-Length: 3\r\n";
+			print $client "\r\n";
+			print $client "404";
 		}
-		close FILE1;
-		print $client "HTTP/1.0 200 OK\r\n";
-		print $client "Content-Type: text/html; charset=utf-8\r\n";
-		printf $client "Content-Length: %d\r\n",length($page);
-		print $client "\r\n";
-		print $client $page;
 	} else {
 	}
 	close $client;
